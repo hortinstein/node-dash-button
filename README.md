099cc54648ca6bc2aff0292e465dcff88c30d744
hortinstein <hortinstein@gmail.com>
2015-09-01 03:28:12 +0000

    adding files

--- README.md
+++ README.md
@@ -1,6 +1,25 @@
-# dash-button
-Place it. Hack it. Press it.
-
 ![Blank Dash](http://i.imgur.com/V26dxTl.png)
+# dash-button
+Place it. *Hack it.* Press it. ~~Get it.~~
 
-Dependencies: libpcap
+This module was inspired by [this fantastic article by Edward Bensen](https://medium.com/@edwardbenson/how-i-hacked-amazon-s-5-wifi-button-to-track-baby-data-794214b0bdd8).
+
+It is a simple library that will allow you to utilize a dash button to initiate a callback.  I am using the same strategy of watching for dash generated ARP requests as the article above.
+
+Contents
+-----------------
+- [Installation Instructions](#installation-instructions)
+- [Usage](#usage)
+ 
+# Installation Instructions: 
+```
+# dependancy on libpcap for reading packets
+sudo apt-get install libpcap-dev
+# currently not on npm
+npm install dash-button 
+```
+
+# Usage:
+```
+
+```
--- index.js
+++ index.js
@@ -1,37 +1,45 @@
-var arp = require('arpjs');
+// pcap_session.on('packet', function (raw_packet) {
+//     var packet = pcap.decode.packet(raw_packet);
+//     if (packet.payload.ethertype === 2054){
+//       console.log("arp detected");
+//       console.log(packet.payload.payload.sender_ha)
+//       console.log(packet.payload.payload.target_ha)
+//     }
+//     arp.table(function(err, table){
+//       console.log(table);
+//     });
+// });
+// 
+var _ = require("underscore")
+var dash_button = [];
+module.exports = dash_button;
 
-var sendarp = function(){
-  arp.send({
-  'op': 'request',
-  'src_ip': '10.105.50.100',
-  'dst_ip': '10.105.50.1',
-  'src_mac': '8f:3f:20:33:54:44',
-  'dst_mac': 'ff:ff:ff:ff:ff:ff'
-  });  
-};
-
-setInterval(sendarp, 1000); 
-var pcap = require('pcap'),
-    pcap_session = pcap.createSession();
-
-pcap_session.on('packet', function (raw_packet) {
-    var packet = pcap.decode.packet(raw_packet);
-    if (packet.payload.ethertype === 2054){
-      console.log("arp detected");
-      console.log(packet.payload.payload.sender_ha)
-      console.log(packet.payload.payload.target_ha)
+dash_button.hex_to_int_array = function(hex){
+    var hex_array = hex.split(":");
+    var int_array = [];
+    for (var i in hex_array) {
+        int_array.push( parseInt(hex_array[i], 16));
     }
-    arp.table(function(err, table){
-      console.log(table);
-    });
-});
+    return (int_array); 
+}
 
-// var dash_button = {};
-// modules.exports = dash_button;
 
-// var registered_dash_button = {};
-
-// dash_button.register = function(mac_address, callback){
-    
+// var dash_button.register = function(mac_address, callback) {
+//     var pcap = require('pcap'),
+//         pcap_session = pcap.createSession();
+//     console.log(mac_address);
+//     return {
+//         pcap_session.on('packet', function(raw_packet) {
+//             var packet = pcap.decode.packet(raw_packet);
+//             if(packet.payload.ethertype === 2054) {
+//                 if(_.isEqual(packet.payload.payload.sender_ha, dash_button.hex_to_int_array(mac_address))) 
+//                     callback()
+//                     console.log("arp detected");
+//                     console.log(packet.payload.payload.sender_ha)
+//                     console.log(packet.payload.payload.target_ha)
+//                 }
+//         });
+//     };
+// };
+// callback(null, "complete");
 // }
-
