488fcf8037b8164097d236a55865a1e4352cd7f0
hortinstein <hortinstein@gmail.com>
2015-09-01 21:00:13 +0000

    rewrote it as an event emitter

--- index.js
+++ index.js
@@ -2,10 +2,8 @@ var pcap = require('pcap'),
     pcap_session = pcap.createSession();
 
 var _ = require("underscore")
-var dash_button = [];
-module.exports = dash_button;
 
-dash_button.hex_to_int_array = function(hex){
+var hex_to_int_array = function(hex){
     var hex_array = hex.split(":");
     var int_array = [];
     for (var i in hex_array) {
@@ -14,25 +12,32 @@ dash_button.hex_to_int_array = function(hex){
     return (int_array); 
 }
 
-dash_macs = [];
 
-dash_button.register = function(mac_address) {
-    dash_button.push(mac_address);
+
+var stream = require('stream')
+
+var register = function(mac_address) {
+    var readStream = new stream.Readable({
+        objectMode: true
+    });
+    pcap_session.on('packet', function(raw_packet) {
+        var packet = pcap.decode.packet(raw_packet); //decodes the packet
+        if(packet.payload.ethertype === 2054) { //ensures it is an arp packet
+            console.log(hex_to_int_array(mac_address));
+            console.log(packet.payload.payload.sender_ha);
+            if(_.isEqual(packet.payload.payload.sender_ha, dash_button.hex_to_int_array(mac_address))) {
+                readStream.emit('detected');
+            }	
+        }
+    });
+    
+    return readStream
 };
 
-pcap_session.on('packet', function(raw_packet) {
-    var packet = pcap.decode.packet(raw_packet);
-    if(packet.payload.ethertype === 2054) {
-    	for (var i in dash_macs){
-    		console.log(dash_button.hex_to_int_array(dash_macs[i]));
-    		console.log(packet.payload.payload.sender_ha);
-	    	if(_.isEqual(packet.payload.payload.sender_ha, dash_button.hex_to_int_array(dash_macs[i]))) {
-	        	console.log("arp detected");
-	            console.log(packet.payload.payload.sender_ha);
-	            console.log(packet.payload.payload.target_ha);
-	        	dash_macs[i](null,mac_address);
-	        }	
-    	}
-         
-    }
-});
+if (process.env.NODE_ENV === 'test') {
+  module.exports = { hex_to_int_array: hex_to_int_array, 
+                     register: register
+                   };
+} else {
+  module.exports = register;
+}
--- test/test.js
+++ test/test.js
@@ -1,7 +1,9 @@
+process.env.NODE_ENV = 'test'
+
 var should = require('should');
 var assert = require('assert');
 var dash_button = require('../index.js');
-
+console.log(process.env.NODE_ENV)
 var arp = require('arpjs');
 var sendarp = function(){
   arp.send({
@@ -18,18 +20,20 @@ startTests = function() {
         
         done();
     });
-    it('should recognize an arp request', function(done) {
-    	setInterval(sendarp, 100);    
-        dash_button.register("8f:3f:20:33:54:44");
-        done();
-        
-    });
     it('should correctly convert string hex to decimal array', function(done) {
         console.log(dash_button.hex_to_int_array('8f:3f:20:33:54:44'));
         
        done(); 
     });
-
+    it('should recognize an arp request', function(done) {
+    	//setInterval(sendarp, 100);    
+        dash_button.register("8f:3f:20:33:54:44").on('detected', function(){
+        
+        });
+        done();
+        
+    });
+    
 }
 startTests();
 
