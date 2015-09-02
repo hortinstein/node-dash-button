0bf0e84d26868768a5bc388b8c4896c32dc29b9f
Alex Hortin <hortinstein@gmail.com>
2015-09-01 22:27:08 -0400

    ready for commit and publish to NPM, need to test on a computer with access to network, vms are not seeing over arp requests

--- index.js
+++ index.js
@@ -1,21 +1,41 @@
 var pcap = require('pcap'),
     pcap_session = pcap.createSession();
 
-var _ = require("underscore")
+var _ = require("underscore");
 
+// converts a string: "8f:3f:20:33:54:44"
+// to a numeric array: [ 143, 63, 32, 51, 84, 68 ]
+// for comparison
 var hex_to_int_array = function(hex){
     var hex_array = hex.split(":");
     var int_array = [];
     for (var i in hex_array) {
         int_array.push( parseInt(hex_array[i], 16));
     }
-    return (int_array); 
+    //console.log(hex,int_array)
+    return int_array; 
+
+}
+
+// converts a numeric array: [ 143, 63, 32, 51, 84, 68 ]
+// to a string: "8f:3f:20:33:54:44"=
+// for comparison
+var int_array_to_hex = function (int_array) {
+    var hex = "";
+    for (var i in int_array){
+        var h = int_array[i].toString(16); // converting to hex
+        if (h.length < 2) h = '0' + h; //adding a 0 for non 2 digit numbers
+        if (i !== int_array.length) hex+=":"; //adding a : for all but the last group
+        hex += h;
+    }
+    return hex.slice(1);//slice is to get rid of the leading :
 }
 
 
 
-var stream = require('stream')
+var stream = require('stream');
 
+//Function to register the node button
 var register = function(mac_address) {
     var readStream = new stream.Readable({
         objectMode: true
@@ -23,21 +43,20 @@ var register = function(mac_address) {
     pcap_session.on('packet', function(raw_packet) {
         var packet = pcap.decode.packet(raw_packet); //decodes the packet
         if(packet.payload.ethertype === 2054) { //ensures it is an arp packet
-            console.log(hex_to_int_array(mac_address));
-            console.log(packet.payload.payload.sender_ha);
-            if(_.isEqual(packet.payload.payload.sender_ha, dash_button.hex_to_int_array(mac_address))) {
+            if(_.isEqual(packet.payload.payload.sender_ha.addr, 
+                         hex_to_int_array(mac_address))) {
                 readStream.emit('detected');
             }	
         }
     });
-    
-    return readStream
+    return readStream;
 };
 
 if (process.env.NODE_ENV === 'test') {
-  module.exports = { hex_to_int_array: hex_to_int_array, 
-                     register: register
-                   };
+    module.exports = {  hex_to_int_array: hex_to_int_array, 
+                        int_array_to_hex: int_array_to_hex,
+                        register: register
+                    };
 } else {
-  module.exports = register;
+    module.exports = register;
 }
--- test/test.js
+++ test/test.js
@@ -1,39 +1,42 @@
-process.env.NODE_ENV = 'test'
+process.env.NODE_ENV = 'test';
 
 var should = require('should');
 var assert = require('assert');
 var dash_button = require('../index.js');
-console.log(process.env.NODE_ENV)
+
+var hex = '8f:3f:20:33:54:44';
+var int_array = [];
+
 var arp = require('arpjs');
 var sendarp = function(){
-  arp.send({
-  'op': 'request',
-  'src_ip': '10.105.50.100',
-  'dst_ip': '10.105.50.1',
-  'src_mac': '8f:3f:20:33:54:44',
-  'dst_mac': 'ff:ff:ff:ff:ff:ff'
-  });  
+    arp.send({
+    'op': 'request',
+    'src_ip': '10.105.50.100',
+    'dst_ip': '10.105.50.1',
+    'src_mac': hex,
+    'dst_mac': 'ff:ff:ff:ff:ff:ff'
+    });  
 };
 
 startTests = function() {
     before(function(done) {
-        
-        done();
+        done(); 
     });
     it('should correctly convert string hex to decimal array', function(done) {
-        console.log(dash_button.hex_to_int_array('8f:3f:20:33:54:44'));
-        
-       done(); 
+        int_array = dash_button.hex_to_int_array(hex)
+        done(); 
+    });
+    it('should correctly convert a decimal array to string hex', function(done) {
+        dash_button.int_array_to_hex(int_array).should.equal(hex);
+        done(); 
     });
     it('should recognize an arp request', function(done) {
-    	//setInterval(sendarp, 100);    
-        dash_button.register("8f:3f:20:33:54:44").on('detected', function(){
-        
-        });
-        done();
-        
+        this.timeout(30000);//sometimes the detection takes a while
+    	  setTimeout(sendarp, 2500); //giving pcap time to set up a listener   
+        dash_button.register(hex).on('detected', function(){
+            done();
+        });        
     });
-    
 }
 startTests();
 
